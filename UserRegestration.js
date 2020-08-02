0920a8132699f80d469e1fba70d33c84530754c6
Sudhanshu Ghinmine <sghinmine54@gmail.com>
2020-08-03 00:10:50 +0530

    REFACTOR Regex for password to add rule for minimum one upper case char

--- UserRegestration.js
+++ UserRegestration.js
@@ -14,8 +14,8 @@ var mobileArray = ["91 8551973494", "8551973494", "91 855197", "918551973494"];
 var mobilePattern = /^[1-9]+[0-9]+[\ ]+[1-9]+[0-9]{9}$/;
 validate(mobilePattern, mobileArray);
 
-var passwordArray = ["sudhanshu", "sudh"];
-var passwordPattern = /[0-9A-Za-z]{8,}$/;
+var passwordArray = ["Sudhanshu", "sudhanshu", "Sudh"];
+var passwordPattern = /(?=[^A-Z]*[A-Z])[0-9A-Za-z]{8,}$/;
 validate(passwordPattern, passwordArray);
 
 function validate(pattern, array) {
