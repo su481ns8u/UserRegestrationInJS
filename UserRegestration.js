8d6b0dcf6466ddc1142e4f7b019fd4f884f90b1a
Sudhanshu Ghinmine <sghinmine54@gmail.com>
2020-08-02 23:59:09 +0530

    ADDED Regex validation for last name

--- UserRegestration.js
+++ UserRegestration.js
@@ -2,6 +2,10 @@ var firstNamesArray = ["Sudhanshu", "Sud", "sudhanshu", "Su"];
 var firstNamePattern = /^[A-Z]{1}[a-z]{2,}/;
 validate(firstNamePattern, firstNamesArray);
 
+var lastNamesArray = ["Ghinmine", "Ghi", "ghinmine", "Gh"];
+var lastNamePattern = /^[A-Z]{1}[a-z]{2,}/;
+validate(lastNamePattern, lastNamesArray);
+
 function validate(pattern, array) {
   for (var i = 0; i < array.length; i++) {
     if (pattern.test(array[i])) console.log(array[i] + ": Valid");
