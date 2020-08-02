b4b882dbbf3129e0e21997f6038c35032c9b8b2a
Sudhanshu Ghinmine <sghinmine54@gmail.com>
2020-08-03 00:01:11 +0530

    ADDED Regex validation for email

--- UserRegestration.js
+++ UserRegestration.js
@@ -6,6 +6,10 @@ var lastNamesArray = ["Ghinmine", "Ghi", "ghinmine", "Gh"];
 var lastNamePattern = /^[A-Z]{1}[a-z]{2,}/;
 validate(lastNamePattern, lastNamesArray);
 
+var emailsArray = ["abc.xyz@bl.co.in"];
+var emailPattern = /^[0-9A-Za-z]+(([._+-]*)[0-9A-Za-z]+)*@[0-9A-Za-z]+.[a-z]{2,4}([.][a-z]{2,3})*$/;
+validate(emailPattern, emailsArray);
+
 function validate(pattern, array) {
   for (var i = 0; i < array.length; i++) {
     if (pattern.test(array[i])) console.log(array[i] + ": Valid");
